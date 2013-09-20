8c952374e03b781c9662978dfce1596eead5f48b
Clay Stewart <claytonmstewart@gmail.com>
2013-09-20 12:17:42 -0500

    Finalize Gemfile

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,7 +1,9 @@
 require 'spec_helper'
 
 describe "StaticPages" do
-  
+
+  let(:base_title) { "Game Starter" }
+
   describe "Home page" do
 
     it "should have the content 'Game On'" do
@@ -11,7 +13,7 @@ describe "StaticPages" do
 
     it "should have the right title" do
       visit '/static_pages/home'
-      expect(page).to have_title("Game Starter | Home")
+      expect(page).to have_title("#{base_title} | Home")
     end
   end
 
@@ -23,7 +25,7 @@ describe "StaticPages" do
     end
     it "should have the right title" do
       visit '/static_pages/help'
-      expect(page).to have_title("Game Starter | Help")
+      expect(page).to have_title("#{base_title} | Help")
     end
   end
 
@@ -35,7 +37,7 @@ describe "StaticPages" do
     end
     it "should have the right title" do
       visit '/static_pages/about'
-      expect(page).to have_title("Game Starter | About")
+      expect(page).to have_title("#{base_title} | About")
     end
   end
 
@@ -47,7 +49,7 @@ describe "StaticPages" do
     end
     it "should have the right title" do
       visit '/static_pages/contact'
-      expect(page).to have_title("Game Starter | Contact")
+      expect(page).to have_title("#{base_title} | Contact")
     end
   end
 end
