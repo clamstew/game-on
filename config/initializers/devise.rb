43c84e482ecb4e21d4fcc8da2662f8cb3971bbd5
Clay Stewart <claytonmstewart@gmail.com>
2013-09-20 16:01:12 -0500

    comment out devise gem

---
+++ config/initializers/devise.rb
@@ -0,0 +1,3 @@
+# Devise.setup do |config|
+#   config.secret_key = '3874c30d7f32111483ff45d26e4741afd411d2c3b044c54c314baef17dc49da3cdc648e663fa8043b7e18a136bab96219f8ac0d689e8ec124ca092c2eabd8e07'
+# end
